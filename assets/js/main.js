441bfbd4e74b23073bee57a98a8871bc4bfa5c64
Natalie Tang <natalietang77@gmail.com>
2022-07-27 23:42:10 -0500

    added toggle to show menu

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/*================== MENU SHOW Y HIDDEN ==================*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+/*================== MENU SHOW ==================*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+
+/*================== MENU HIDDEN ==================*/
+/* Validate if constant exists */
+
+/*================== REMOVE MENU MOBILE ==================*/
+
+/*================== ACCORDION SKILLS ==================*/
+
+/*================== QUALIFICATION TABS ==================*/
+
+/*================== SERVICES MODAL ==================*/
